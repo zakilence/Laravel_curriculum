370cd9456671ff908e5a84340e487e9147a71c98
zakilence <kosakana11@gmail.com>
2021-09-01 11:58:10 +0900

    first

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+    }
+}
