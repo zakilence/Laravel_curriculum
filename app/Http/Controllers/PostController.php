a25d6a7e6a9841d783ff0d5829d2da641e58935a
zakilence <kosakana11@gmail.com>
2021-09-01 17:14:15 +0900

    good commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return $post->get();
     }
 }
