6fea7641fd1fb0bfceca87ff1ca318de1d459ef5
zakilence <kosakana11@gmail.com>
2021-09-02 22:37:28 +0900

    blog投稿バリデーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -19,7 +19,7 @@ class PostController extends Controller
     {
         return view('create');
     }
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
