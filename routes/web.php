9866f5fe02316ff70c8d1c678730245833108696
zakilence <kosakana11@gmail.com>
2021-09-06 12:20:56 +0900

    ブログ削除機能

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/posts/{post}','PostController@show');
 Route::get('/posts/{post}/edit','PostController@edit');
 Route::put('/posts/{post}','PostController@update');
 Route::post('/posts','PostController@store');
+Route::delete('/posts/{post}','PostController@delete');
